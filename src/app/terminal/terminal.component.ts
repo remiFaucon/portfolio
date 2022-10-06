40c8c26ed3f904b1de4ccc804d0ac7098ce29738
faucon <faucon.remi04@gmail.com>
2022-10-06 10:08:40 +0200

    terminal

--- src/app/terminal/terminal.component.ts
+++ src/app/terminal/terminal.component.ts
@@ -1,6 +1,15 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  ApplicationRef,
+  Component, ComponentFactory,
+  ComponentFactoryResolver,
+  Injector,
+  OnInit,
+  ViewContainerRef,
+  ViewEncapsulation
+} from '@angular/core';
 import {Router} from '@angular/router'
 import {isEmpty, throwIfEmpty} from "rxjs";
+import {element} from "three/examples/jsm/nodes/shadernode/ShaderNodeBaseElements";
 
 export interface character {
   val: string
@@ -15,76 +24,55 @@ export interface character {
 })
 export class TerminalComponent implements OnInit {
   terminalContent = new Map()
-  toggle: boolean = true
-  cssClass: string = "hidden"
-  constructor(private router: Router) {
-    // this.terminalContent.set(1, )
-  }
+  constructor(private router: Router) {}
   ngOnInit(): void {
     (async () => {
-      this.showTerminalContent().then(() => {
-        let toggleElement = document.querySelector("app-terminal .terminal div:last-of-type a span:last-of-type")!;
-        setInterval(() => {
-          this.toggle = !this.toggle
-        }, 750)
-      })
-
-      document.addEventListener("keydown", async (e) => {
-        const input = lastText.querySelector("#input")!
-        if (e.key === "Y" || e.key === "y") {
-          input.innerHTML = input.innerHTML + e.key
-          setTimeout(() => {
-            this.router.navigateByUrl('/propos')
-          }, 500)
-        } else if (e.key === "n" || e.key === "N") {
-          input.innerHTML = input.innerHTML + e.key
-          const newCommand = document.createElement("p")
-          const terminal = document.querySelector(".terminal")!
-          newCommand.classList.add("commandInput")
-          newCommand.innerHTML = "<span class=\"green\">user@machine:</span><span class=\"blue\">~</span>$ <span id=\"toShow\"></span>"
-          terminal.appendChild(newCommand)
-          // await this.showingCommand("sudo rm -rf /", newCommand)
-        }
-      })
+      await this.showTerminalContent()
+        setTimeout(() =>{
+          let input = document.querySelector("app-terminal .terminal div:last-of-type p span:last-of-type")!;
+          let toggle = true
+          setInterval(() => {
+            if (toggle)
+              input.classList.toggle("hiddenInput");
+            else{
+              input.classList.add("hiddenInput");
+              return
+            }
+          }, 750)
+          document.addEventListener("keydown", async (e) => {
+            e.preventDefault()
+            if (action(e.key) === "inner") {
+              input.innerHTML = input.innerHTML + e.key
+            } else if (action(e.key) === "outer") {
+              input.innerHTML = input.innerHTML.slice(0, -1)
+            } else if (action(e.key) === "enter") {
+              toggle = false
+              if (input.innerHTML === "home") {
+                await this.router.navigate(["/"])
+              } else if (input.innerHTML === "service") {
+                await this.router.navigate(["/services"])
+              } else if (input.innerHTML === "project") {
+                await this.router.navigate(["/realisation"])
+              } else if (input.innerHTML === "about") {
+                await this.router.navigate(["/a-propos"])
+              } else if (input.innerHTML === "sudo rm -rf /") {
+                await this.deletePage().then(() => this.router.navigate(["/contact"]))
+                await this.router.navigate(["/contact"])
+              }
+            }
+          })
+        }, 1)
     }) ()
-    const lastText = document.querySelector(".terminal .hidden:last-child")!;
 
-    // })()
+    function action(key: string): string|void {
+      let letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "-", "/", " "]
+      if(letter.includes(key)) return "inner"
+      else if (key === "Backspace") return "outer"
+      else if (key === "Enter") return "enter"
+    }
   }
 
-  // showingCommand(toShow: string, whereShow: HTMLElement): Promise<any> {
-  //   const characters = toShow.split("")
-  //   return new Promise((successCallback) => {
-  //     setTimeout(() => {
-  //       let showed: string = ""
-  //       characters.forEach((character, index) => {
-  //         setTimeout(() => {
-  //           showed += character
-  //           if (character === " ")
-  //             character = "&nbsp;"
-  //           whereShow.innerHTML = whereShow.innerHTML + character
-  //           if (showed.length === toShow.length)
-  //             successCallback("commandSowing")
-  //         }, index * 100)
-  //       })
-  //      }, 2000)
-  //   })
-  // }
-  //
-  // showingText(lastText: Element): Promise<string> {
-  //   return new Promise((successCallback) => {
-  //     const text = document.querySelectorAll(".terminal .hidden:not(:last-child)")
-  //     setTimeout(() => {
-  //       text.forEach(element => element.classList.remove("hidden"))
-  //     }, 1000)
-  //     setTimeout(() => {
-  //       lastText.classList.remove("hidden")
-  //       successCallback("textShowing")
-  //     }, 1500)
-  //   })
-  // }
-
-  async addLine(text: string, line: number, writeSpeed:number = 0, writeAtEnd:number = 0, timeOut = 0): Promise<void>{
+  private async addLine(text: string, line: number, writeSpeed:number = 0, writeAtEnd:number = 0, timeOut = 0): Promise<void>{
     if (!line)
       line = this.terminalContent.size
     const promise = new Promise<void>((resolve) => {
@@ -121,28 +109,44 @@ export class TerminalComponent implements OnInit {
     return await promise
   }
 
-  async showTerminalContent():Promise<void> {
-      await this.addLine("<p class=\"c\"><span class=\"green\">user@machine:</span><span class=\"blue\">~</span>$ </p>", 1)
-      await this.addLine("portfolio --config", 1, 100, 4, 1000)
-      await this.addLine("<h1>Rémi Faucon,<strong>Developpeur back-end</strong></h1>", 2,0, 0, 1000)
-      await this.addLine("<p>Passionné par l’univers du développement depuis mes jeunes âges, j’ai débuté par les langages fondamentaux du web. </p>",
-        3,
-        0,
-        0,
-        0
-      )
-      await this.addLine("<p>J’ai ensuite approfondi mes connaissances en back-end notamenent en PHP, puis j’ai suivi une formation diplomante d’un titre développeur web et web mobile (bac+2).</p>",
-        4,
-        0,
-        0,
-        0
-      )
-      await this.addLine("<a><span class=\"green\">?</span> Découvrir mes réalisations? <span class=\"grey\">(Y/n)</span><span id=\"input\"></span><p [*ngIf]='false'> _</p></a>",
-        5,
-        0,
-        0,
-        1000
-      )
+  private async showTerminalContent():Promise<void> {
+    await this.addLine("<p><span class=\"green\">user@machine:</span><span class=\"blue\">~</span>$ </p>", 1)
+    await this.addLine("home", 1, 200, 4, 1000)
+    await this.addLine(
+      "<p>Commands:</p>" +
+      "<div class='commandsList'>" +
+      "<div class='commands'>" +
+      "<p>home</p>" +
+      "<p>service</p>" +
+      "<p>project</p>" +
+      "<p>about</p>" +
+      "<p>sudo rm -rf /</p>" +
+      "</div>" +
+      "<div class='commandsDescription'>" +
+      "<p>Go to the home page</p>" +
+      "<p>Do you want a coffee?</p>" +
+      "<p>It's alright, view the matrix</p>" +
+      "<p>About me</p>" +
+      "<p>Please no !</p>" +
+      "</div>" +
+      "</div>", 2, 0, 0, 1000)
+    await this.addLine("<p><span class=\"green\">user@machine:</span><span class=\"blue\">~</span>$ <span class='input'></span></p>", 3, 0, 0, 1000)
     return
+  }
+
+  private async deletePage(): Promise<void> {
+    const promise = new Promise<void>(async (resolve, reject) => {
+      let elements = Array.from(document.querySelectorAll("p, h1, h2, a"))
+      let currentIndex = elements.length
+      while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [elements[currentIndex], elements[randomIndex]] = [elements[randomIndex], elements[currentIndex]];
+      }
+      elements.forEach((element, index) => {
+        setTimeout(() => element.remove(), 300 * index);
+      })
+    })
+    return await promise
   }
 }
